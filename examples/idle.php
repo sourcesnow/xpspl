4ed0ac824660e590199d7dfb2f31bcbd7445bf71
nwhitingx <prggmr@gmail.com>
2012-08-03 15:16:01 -0400

    Idle objects now work need to fix the override

--- examples/idle.php
+++ examples/idle.php
@@ -1,22 +1,26 @@
 <?php
 error_reporting(E_ALL);
 
-echo intval(null);
-exit;
 prggmr\load_signal("time");
 prggmr\handle(function(){
     echo "Loop Starting Here";
 }, new \prggmr\engine\signal\Loop_Start());
-
 prggmr\handle(function(){
-    throw new Exception("HAHA");
+    echo "Loop Ending";
 }, new \prggmr\engine\signal\Loop_Shutdown());
 
-prggmr\signal\time\interval(function(){
-    // echo $this->doesnotexit;
-}, 1500);
+prggmr\handle(function(){
+    // throw new Exception("HAHA");
+}, new \prggmr\engine\signal\Loop_Shutdown());
 
-prggmr\signal\time\timeout(function(){
-    prggmr\shutdown();
-    echo "5 Seconds".PHP_EOL;
-}, 5000);
+// prggmr\signal\time\interval(function(){
+//     echo "1 second".PHP_EOL;;
+// }, 1, \prggmr\engine\idle\Time::SECONDS);
+
+prggmr\signal\time\interval(function(){
+    echo "1 milliseconds".PHP_EOL;
+}, 1, \prggmr\engine\idle\Time::MILLISECONDS);
+
+prggmr\signal\time\interval(function(){
+    echo "1 microsecond".PHP_EOL;
+}, 1, \prggmr\engine\idle\Time::MICROSECONDS);
