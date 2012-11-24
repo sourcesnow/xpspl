e7d12160a56ae042d08ad84a6733fa21266dc614
prggmr <prggmr@gmail.com>
2012-11-23 22:10:07 -0500

    update readme to reflect dev status

--- examples/performance.php
+++ examples/performance.php
@@ -1,8 +1,10 @@
 <?php
+prggmr\save_event_history(true);
+var_dump(prggmr\event_history());
 ini_set('memory_limit', -1);
 $time = microtime(true);
 echo "Start".PHP_EOL;;
-for ($i=0;$i!=100;$i++){
+for ($i=0;$i!=10000;$i++){
     prggmr\handle($i, function(){
     });
 }
@@ -12,9 +14,10 @@ echo PHP_EOL;
 $event = new \prggmr\Event();
 $event->a = 1;
 $time = microtime(true);
-for ($i=0;$i!=100;$i++){
+for ($i=0;$i!=10000;$i++){
     prggmr\signal($i, $event);
 }
+var_dump(prggmr\event_history());
 echo $event->a.PHP_EOL;
 echo "Signal Calls".PHP_EOL;
 echo microtime(true) - $time;
