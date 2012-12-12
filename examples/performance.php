66840edd8d9ac2da3f7ecdb0aae8f8ef8ba14ed7
prggmr <nwhiting@xstudiosinc.com>
2012-12-12 11:30:58 -0500

    Readded performance test

---
+++ examples/performance.php
@@ -0,0 +1,20 @@
+<?php
+ini_set('memory_limit', -1);
+$time = microtime(true);
+echo "Start".PHP_EOL;;
+for ($i=0;$i!=1000;$i++){
+    prggmr\handle($i, function(){
+    });
+}
+echo "Handle Register".PHP_EOL;
+echo microtime(true) - $time;
+echo PHP_EOL;
+$event = new \prggmr\Event();
+$event->a = 1;
+$time = microtime(true);
+for ($i=0;$i!=1000;$i++){
+    prggmr\signal($i, $event);
+}
+echo $event->a.PHP_EOL;
+echo "Signal Calls".PHP_EOL;
+echo microtime(true) - $time;
