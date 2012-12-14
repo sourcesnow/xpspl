0e36ab99e4549508f88c89bcf0698031e536aac1
prggmr <nwhiting@xstudiosinc.com>
2012-12-14 16:12:52 -0500

    No more module namespace, socket module is working nicely

--- examples/performance.php
+++ examples/performance.php
@@ -2,7 +2,7 @@
 ini_set('memory_limit', -1);
 $time = microtime(true);
 echo "Start".PHP_EOL;;
-for ($i=0;$i!=10000;$i++){
+for ($i=0;$i!=100000;$i++){
     prggmr\handle($i, function(){
     });
 }
@@ -12,7 +12,7 @@ echo PHP_EOL;
 $event = new \prggmr\Event();
 $event->a = 1;
 $time = microtime(true);
-for ($i=0;$i!=10000;$i++){
+for ($i=0;$i!=100000;$i++){
     prggmr\signal($i, $event);
 }
 echo $event->a.PHP_EOL;
---
+++ examples/sockets/echo_server.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright 2010-12 Nickolas Whiting. All rights reserved.
+ * Use of this source code is governed by the Apache 2 license
+ * that can be found in the LICENSE file.
+ */
+
+/**
+ * Echo Server
+ *
+ * This example demonstrates a simple echo server that spits back anything that
+ * was sent and then disconnects.
+ */
+prggmr\load_module('socket');
+
+$socket = prggmr\socket('0.0.0.0', ['port' => '80'], function(){
+    echo "Server Running on " . $this->socket->get_address() . PHP_EOL;
+});
+
+$socket->on_connect(function(){
+    echo "Connection " . PHP_EOL;
+    $this->socket->write($this->socket->read());
+    $this->socket->disconnect();
+});
